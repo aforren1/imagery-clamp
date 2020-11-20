7f6951bb098d30cb6c060685a9cb5009c7799f59
Alex Forrence <alex.forrence@gmail.com>
2020-11-19 22:40:16 -0500

    tweak sd, add cursor image

--- src/scripts/utils/std.js
+++ src/scripts/utils/std.js
@@ -1,8 +1,8 @@
 // standard deviation
 // https://stackoverflow.com/a/53577159/2690232
-
+// except n-1 denom to match R
 export default function std(array) {
   const n = array.length
   const mean = array.reduce((a, b) => a + b) / n
-  return Math.sqrt(array.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+  return Math.sqrt(array.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / (n - 1))
 }
